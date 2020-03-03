a146e2b5fd8fdf8b9799f881e48f3594587f4771
Emaan Hariri <emaanhariri@calvisitor-10-105-135-210.calvisitor.1918.berkeley.edu>
2020-03-02 18:09:58 -0800

    Starter solver

--- starter/parse.py
+++ starter/parse.py
@@ -7,8 +7,8 @@ os.chdir(os.path.dirname(os.path.realpath(__file__)))
 if not os.path.exists('in'): os.mkdir('in')
 if not os.path.exists('out'): os.mkdir('out')
 
-def parse_input_file(file):
-    with open(file, 'r') as fo:
+def read_input_file(path):
+    with open(path, 'r') as fo:
         n = int(fo.readline())
         lines = fo.read().splitlines()
         fo.close()
@@ -16,16 +16,16 @@ def parse_input_file(file):
         G.add_nodes_from(range(n))
         return G
 
-def write_input_file(G, file):
-    with open(file, 'w') as fo:
+def write_input_file(G, path):
+    with open(path, 'w') as fo:
         n = len(G)
         lines = nx.generate_edgelist(G, data=['weight'])
         fo.write(str(n) + '\n')
         fo.writelines('\n'.join(lines))
         fo.close()
 
-def read_output_file(file):
-    with open(file, 'r') as fo: 
+def read_output_file(path):
+    with open(path, 'r') as fo: 
         nodes = fo.readline()
         lines = fo.read().splitlines()
         fo.close()
@@ -33,24 +33,9 @@ def read_output_file(file):
         T.add_nodes_from(map(int, nodes.split()))
         return T
 
-def write_output_file(T, file):
-    with open(file, 'w') as fo: 
+def write_output_file(T, path):
+    with open(path, 'w') as fo: 
         fo.write(' '.join(map(str, T.nodes)) + '\n')
         lines = nx.generate_edgelist(T, data=['weight'])
         fo.writelines('\n'.join(lines))
         fo.close()
-
-G = parse_input_file('in/test.in')
-T = nx.minimum_spanning_tree(G)
-# T.remove_edge(0, 1)
-T.remove_node(0)
-write_output_file(T, 'out/test.out')
-T = read_output_file('out/test.out')
-print(T.nodes)
-
-# G = nx.Graph()
-# G.add_node(1)
-# G.add_node(2)
-# G.add_node(3)
-# G.add_edge(2, 3)    
-# nx.write_weighted_edgelist(G, test)
--- starter/solver.py
+++ starter/solver.py
@@ -1,5 +1,6 @@
 import networkx as nx
-
+from parse import read_input_file, write_output_file 
+import sys
 
 def solve(G):
     """
@@ -11,9 +12,13 @@ def solve(G):
     """
 
     T =  # TODO: your code here!
-    return T
+    return nx.minimum_spanning_tree(G)
 
 
 
 if __name__ == '__main__':
-    pass
+    assert len(sys.argv) == 2
+    path = sys.argv[1]
+    G = read_input_file(path)
+    T = solve(G)
+    write_output_file(T, 'out/test.out')
