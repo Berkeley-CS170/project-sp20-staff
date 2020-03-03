2929f0d95800375ee812682969ee2356e366e4df
David Vendrow <davidv@Admins-MacBook-Pro-4.local>
2020-03-02 16:33:42 -0800

    add basic starter

---
+++ starter/solver.py
@@ -0,0 +1,19 @@
+import networkx as nx
+
+
+def solve(G):
+    """
+    Args:
+        G: networkx.Graph
+
+    Returns:
+        T: networkx.Graph
+    """
+
+    T =  # TODO: your code here!
+    return T
+
+
+
+if __name__ == '__main__':
+    pass
