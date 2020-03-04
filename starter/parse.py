b9b4dd10f76d18a4b59320f55214596d16da5725
David Vendrow <davidv@Admins-MacBook-Pro-4.local>
2020-03-04 15:59:33 -0800

    input validation

--- starter/parse.py
+++ starter/parse.py
@@ -1,5 +1,6 @@
 import numpy as numpy
 import networkx as nx
+import re
 import os
 
 # Set path to cwd; make in/ and out/ dir
@@ -8,13 +9,39 @@ if not os.path.exists('in'): os.mkdir('in')
 if not os.path.exists('out'): os.mkdir('out')
 
 def read_input_file(path):
+    """
+    Parses an input file
+
+    :param path: str, a path
+    :return: networkx Graph is the input is well formed, None otherwise
+    """
     with open(path, 'r') as fo:
-        n = int(fo.readline())
+        n = fo.readline().strip()
+        assert n.isdigit()
+        n = int(n)
+
         lines = fo.read().splitlines()
         fo.close()
-        G = nx.parse_edgelist(lines, nodetype = int, data=(('weight', float),))
+
+        # validate lines
+        for line in lines:
+            tokens = line.split(' ')
+
+            assert len(tokens) == 3
+            assert tokens[0].isdigit()
+            assert tokens[1].isdigit()
+            assert int(tokens[0]) < n
+            assert int(tokens[1]) < n
+            assert bool(re.match(r'(^\d+\.\d{1,3}$|^\d+$)', tokens[2]))
+
+        G = nx.parse_edgelist(lines, nodetype=int, data=(('weight', float),))
         G.add_nodes_from(range(n))
+
+        assert nx.is_connected(G)
+
         return G
+
+
 
 def write_input_file(G, path):
     with open(path, 'w') as fo:
@@ -29,7 +56,7 @@ def read_output_file(path):
         nodes = fo.readline()
         lines = fo.read().splitlines()
         fo.close()
-        T = nx.parse_edgelist(lines, nodetype = int, data=(('weight', float),))
+        T = nx.parse_edgelist(lines, nodetype=int, data=(('weight', float),))
         T.add_nodes_from(map(int, nodes.split()))
         return T
 
